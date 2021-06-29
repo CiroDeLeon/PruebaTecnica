38d57f884d85a12065f0b8c8965bfec8586a3b77
Ciro Cayo De León Burgos <elalumnopc@hotmail.com>
2021-06-29 08:39:46 -0500

    quinto commit

--- src/main/java/com/prueba/tecnica/nisum/pruebatecnica/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/prueba/tecnica/nisum/pruebatecnica/exceptions/ApiExceptionHandler.java
@@ -1,11 +1,18 @@
 package com.prueba.tecnica.nisum.pruebatecnica.exceptions;
 
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+
 
 @ControllerAdvice
 public class ApiExceptionHandler {   
@@ -14,6 +21,7 @@ public class ApiExceptionHandler {
    @ExceptionHandler(Exception.class) 
    public ResponseEntity<?> handleGlobalException(Exception e,WebRequest request){
 	   Mensaje m=new Mensaje(e.getMessage());
+	   System.out.print(m.getMensaje()+" "+e.getClass().getSimpleName());
 	   return new ResponseEntity(m,HttpStatus.INTERNAL_SERVER_ERROR);
    }
    
@@ -21,24 +29,32 @@ public class ApiExceptionHandler {
    @ExceptionHandler(ValidateException.class) 
    public ResponseEntity<?> handleValidateException(ValidateException e,WebRequest request){
 	   Mensaje m=new Mensaje(e.getMessage());
+	   System.out.print(m.getMensaje()+" "+e.getClass().getSimpleName());
 	   return new ResponseEntity(m,HttpStatus.BAD_REQUEST);
    }
    
    @SuppressWarnings({ "rawtypes", "unchecked" })
    @ExceptionHandler(NotFoundException.class) 
-   public ResponseEntity<?> handleValidateException(NotFoundException e,WebRequest request){
+   public ResponseEntity<?> handleNotFoundException(NotFoundException e,WebRequest request){
 	   Mensaje m=new Mensaje(e.getMessage());
+	   System.out.print(m.getMensaje()+" "+e.getClass().getSimpleName());
 	   return new ResponseEntity(m,HttpStatus.NOT_FOUND);
    }
    
    @SuppressWarnings({ "rawtypes", "unchecked" })
-   @ExceptionHandler(AccessDeniedException.class) 
-   public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e,WebRequest request){
+   @ExceptionHandler(IOException.class) 
+   public ResponseEntity<?> handleAccessDeniedException(IOException e,WebRequest request){
 	   Mensaje m=new Mensaje(e.getMessage());
+	   System.out.print(m.getMensaje()+" "+e.getClass().getSimpleName());
 	   return new ResponseEntity(m,HttpStatus.FORBIDDEN);
    }
-   
-   
+   @SuppressWarnings({ "rawtypes", "unchecked" })
+   @ExceptionHandler(ServletException.class) 
+   public ResponseEntity<?> handleServletException(ServletException e,WebRequest request){
+	   Mensaje m=new Mensaje(e.getMessage());
+	   System.out.print(m.getMensaje()+" "+e.getClass().getSimpleName());
+	   return new ResponseEntity(m,HttpStatus.FORBIDDEN);
+   }
    
    
 }
--- src/main/java/com/prueba/tecnica/nisum/pruebatecnica/security/JWTAuthorizationFilter.java
+++ src/main/java/com/prueba/tecnica/nisum/pruebatecnica/security/JWTAuthorizationFilter.java
@@ -8,10 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -40,9 +43,12 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter{
 			}
 			chain.doFilter(request, response);
 		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
-			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);			
 			((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
 			return;
+			
+						
 		}
 	}	
 
