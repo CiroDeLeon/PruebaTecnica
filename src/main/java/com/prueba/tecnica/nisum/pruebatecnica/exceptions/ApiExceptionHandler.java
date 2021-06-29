21ed7a7b9b3d689dc83381a45fab47164a84c237
Ciro Cayo De León Burgos <elalumnopc@hotmail.com>
2021-06-29 00:20:26 -0500

    Tercer commit

--- src/main/java/com/prueba/tecnica/nisum/pruebatecnica/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/prueba/tecnica/nisum/pruebatecnica/exceptions/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.prueba.tecnica.nisum.pruebatecnica.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -29,4 +30,15 @@ public class ApiExceptionHandler {
 	   Mensaje m=new Mensaje(e.getMessage());
 	   return new ResponseEntity(m,HttpStatus.NOT_FOUND);
    }
+   
+   @SuppressWarnings({ "rawtypes", "unchecked" })
+   @ExceptionHandler(AccessDeniedException.class) 
+   public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e,WebRequest request){
+	   Mensaje m=new Mensaje(e.getMessage());
+	   return new ResponseEntity(m,HttpStatus.FORBIDDEN);
+   }
+   
+   
+   
+   
 }
